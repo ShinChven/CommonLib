8152bf052a43e238a4313480cdf408313adfa40f
ShinChven <shinchven@gmail.com>
2014-10-27 10:59:23 +0800

    ViewHolder

---
+++ CommonTools/src/main/java/com/github/ShinChven/lib/CommonTools/ViewHolder.java
@@ -0,0 +1,26 @@
+package com.github.ShinChven.lib.CommonTools;
+
+import android.util.SparseArray;
+import android.view.View;
+
+/**
+ * Created by ShinChven on 2014/9/18.
+ * 反射的ViewHolder，收集自互联网，未能找到原始出处。
+ */
+public class ViewHolder {
+
+    @SuppressWarnings("unchecked")
+    public static <T extends View> T get(View view, int id) {
+        SparseArray<View> viewHolder = (SparseArray<View>) view.getTag();
+        if (viewHolder == null) {
+            viewHolder = new SparseArray<View>();
+            view.setTag(viewHolder);
+        }
+        View childView = viewHolder.get(id);
+        if (childView == null) {
+            childView = view.findViewById(id);
+            viewHolder.put(id, childView);
+        }
+        return (T) childView;
+    }
+}

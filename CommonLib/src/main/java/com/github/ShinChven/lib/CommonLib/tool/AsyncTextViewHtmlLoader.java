7edba608ac6479c08d08176ed90509e9366745ea
ShinChven <shinchven@gmail.com>
2015-05-03 21:45:07 +0800

    add AsyncTextViewHtmlLoader

---
+++ CommonLib/src/main/java/com/github/ShinChven/lib/CommonLib/tool/AsyncTextViewHtmlLoader.java
@@ -0,0 +1,123 @@
+package com.github.ShinChven.lib.CommonLib.tool;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Html;
+import android.text.Spanned;
+import android.widget.TextView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ShinChven on 15/4/16.<p></p>
+ * 异步向TextView中加载html 图片 <p></p>
+ * use like this <p></p>
+ * <pre>
+ *      TextViewHtmlLoader.loadImageAsync(SplashActivity.this, htmlText, html, 20,
+ *              new TextViewHtmlLoader.OnLoadingImageFinishedListener() {
+ *                   @Override
+ *                   public void onImageLoadingFinished(final Spanned htmlBody) {
+ *                      mHandler.post(new Runnable() {
+ *                          @Override
+ *                          public void run() {
+ *                              htmlText.setText(htmlBody);
+ *                          }
+ *                      });
+ *                  }
+ *              });
+ * </pre>
+ */
+public class AsyncTextViewHtmlLoader {
+
+    // use like this
+    //        TextViewHtmlLoader.loadImageAsync(SplashActivity.this, htmlText, html, 20,
+//                new TextViewHtmlLoader.OnLoadingImageFinishedListener() {
+//                    @Override
+//                    public void onImageLoadingFinished(final Spanned htmlBody) {
+//                        mHandler.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                htmlText.setText(htmlBody);
+//                            }
+//                        });
+//                    }
+//                });
+
+
+    public static final int LIMBO = 10;
+
+    /**
+     * @param context
+     * @param htmlText 要加载的TextView，仅用于加载无图片的文字预览
+     * @param html     HTML
+     * @param textSize 字号
+     * @param listener 响应监听，请在此监听中将返回的Spanned 手动填充到TextView中去
+     */
+    public static void loadImageAsync(final Context context, final TextView htmlText,
+                                      final String html, int textSize,
+                                      final AsyncTextViewHtmlLoader.OnLoadingImageFinishedListener listener) {
+        final Map<String, Drawable> drawablePool = new HashMap<>();
+
+        Spanned fromHtml = Html.fromHtml(html, new Html.ImageGetter() {
+            @Override
+            public Drawable getDrawable(String source) {
+                // todo 先不加载图片
+                return null;
+            }
+        }, null);
+        htmlText.setText(fromHtml);
+
+        if (textSize <= 0) {
+            htmlText.setTextSize(20);
+        } else {
+            htmlText.setTextSize(textSize);
+        }
+        // 完成之后开起线程加载图片
+        // todo 未完成功能：改用AsyncTask中的postExecute方法直接填充数据到TextView，而不需要再将spanned返回出去
+        new Thread(new Runnable() {
+
+            int count = 0;
+
+            @Override
+            public void run() {
+                final Spanned full = Html.fromHtml(html, new Html.ImageGetter() {
+                    @Override
+                    public Drawable getDrawable(String source) {
+                        count = count + 1;
+                        if (count > LIMBO) { // 这个工具并不完善，没有处理性能上的问题，先限制图片个数，保证不会oom
+                            LogTool.i("count", String.valueOf(count));
+                            return null;
+                        }
+
+                        // todo use universal image loader to manage memory
+                        if (drawablePool.containsKey(source)) {
+                            return drawablePool.get(source);
+                        }
+                        // todo load your image async here
+                        ImageLoader loader = UniversalImageLoaderAssist.getLoader(context);
+                        Bitmap bp = loader.loadImageSync(source);
+                        Drawable drawable = new BitmapDrawable(context.getResources(), bp);
+                        int width = htmlText.getMeasuredWidth();
+                        DisplayTool.DisplayMatrix matrix = DisplayTool.zoomWithWidth(width, bp.getWidth(), bp.getHeight());
+                        drawable.setBounds(0, 0, matrix.width, matrix.height);
+                        return drawable;
+                    }
+                }, null);
+
+                listener.onImageLoadingFinished(full);
+
+            }
+        }).start();
+
+    }
+
+    public interface OnLoadingImageFinishedListener {
+        void onImageLoadingFinished(Spanned htmlBody);
+    }
+
+}
+
---
+++ CommonLib/src/main/java/com/github/ShinChven/lib/CommonLib/tool/UniversalImageLoaderAssist.java
@@ -0,0 +1,123 @@
+package com.github.ShinChven.lib.CommonLib.tool;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.decode.BaseImageDecoder;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.SimpleBitmapDisplayer;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
+import java.io.File;
+
+/**
+ * Created by ShinChven on 15/4/15.
+ */
+public class UniversalImageLoaderAssist {
+
+    /**
+     * 快速获得默认设置
+     *
+     * @param context
+     * @return
+     */
+    public static ImageLoaderConfiguration loadQuickConfiguration(Context context) {
+        File cacheDir = StorageUtils.getCacheDirectory(context);
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
+                .diskCacheExtraOptions(480, 800, null)
+                .threadPoolSize(3) // default
+                .threadPriority(Thread.NORM_PRIORITY - 2) // default
+                .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .memoryCacheSizePercentage(13) // default
+                .diskCache(new UnlimitedDiscCache(cacheDir)) // default
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+                .imageDownloader(new BaseImageDownloader(context)) // default
+                .imageDecoder(new BaseImageDecoder(false)) // default
+                .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+                .writeDebugLogs()
+                .build();
+        return config;
+    }
+
+
+    /**
+     * 快速获得默认options
+     * @return
+     */
+    public static DisplayImageOptions loadQuickOptions() {
+
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+//                .showImageOnLoading(R.drawable.ic_stub) // resource or drawable
+//                .showImageForEmptyUri(R.drawable.ic_empty) // resource or drawable
+//                .showImageOnFail(R.drawable.ic_error) // resource or drawable
+                .resetViewBeforeLoading(false)  // default
+                .delayBeforeLoading(1000)
+                .cacheInMemory(false) // default
+                .cacheOnDisk(false) // default
+//                .preProcessor(...)
+//                .postProcessor(...)
+//                .extraForDownloader(...)
+                .considerExifParams(false) // default
+                .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2) // default
+                .bitmapConfig(Bitmap.Config.ARGB_8888) // default
+//                .decodingOptions(...)
+                .displayer(new SimpleBitmapDisplayer()) // default
+                .handler(new Handler()) // default
+                .build();
+        return options;
+    }
+
+    public static ImageLoader getLoader(Context context) {
+        RoundedBitmapDisplayer displayer = new RoundedBitmapDisplayer(10, 10);
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(android.R.drawable.ic_menu_report_image)
+                .showImageForEmptyUri(android.R.drawable.ic_menu_report_image)
+                .showImageOnFail(android.R.drawable.ic_menu_report_image)
+                .cacheOnDisk(true)
+                .considerExifParams(true)
+                .displayer(displayer)
+                .imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+                .build();
+
+        File cacheDir = StorageUtils.getCacheDirectory(context);
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
+                .diskCacheExtraOptions(480, 800, null)
+                .threadPoolSize(3) // default
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize( 1024 * 1024)
+                .memoryCacheSizePercentage(13) // default
+                .diskCache(new UnlimitedDiscCache(cacheDir)) // default
+                .imageDownloader(new BaseImageDownloader(context)) // default
+                .imageDecoder(new BaseImageDecoder(false)) // default
+                .threadPriority(Thread.NORM_PRIORITY - 2) // default
+                .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator()) // default
+                .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+                .writeDebugLogs()
+                .build();
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(config);
+        return imageLoader;
+    }
+
+}

962a563acce5e2ca9bfc2767ce8beffbd034c1f3
ShinChven <shinchven@gmail.com>
2015-05-08 23:07:50 +0800

    md5

---
+++ CommonLib/src/main/java/com/github/ShinChven/lib/CommonLib/utils/MD5.java
@@ -0,0 +1,28 @@
+package com.github.ShinChven.lib.CommonLib.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by ShinChven on 15/5/6.
+ */
+public class MD5 {
+
+    public static String encrypt(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+        }
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
+}

bf0ee4bb3f7baa69e58c770619d04d8064ce148b
ShinChven <shinchven@gmail.com>
2015-06-24 22:42:26 +0800

    update BitmapUtil from github website

--- CommonLib/src/main/java/com/github/ShinChven/lib/CommonLib/utils/BitmapUtil.java
+++ CommonLib/src/main/java/com/github/ShinChven/lib/CommonLib/utils/BitmapUtil.java
@@ -18,20 +18,21 @@ import java.io.IOException;
  * Created by ShinChven on 2014/10/21.
  */
 public class BitmapUtil {
-    private static final String SCHEME_CONTENT = "content";
-    private static final String SCHEME_FILE = "file";
+    public static final String SCHEME_CONTENT = "content";
+    public static final String SCHEME_FILE = "file";
 
-    public static Bitmap zip(Context context, Uri uri,
-                             int reqWidth, int reqHeight) throws IOException {
+    public static Bitmap resize(Context context, Uri uri,
+                                int reqWidth, int reqHeight) throws IOException {
 
         // First decode with inJustDecodeBounds=true to check dimensions  开启测量模式
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
 
-        // SCHEME 判断类型
         if (uri.getScheme().equals(SCHEME_CONTENT)) {
             BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options);
         } else if (uri.getScheme().equals(SCHEME_FILE)) {
+            BitmapFactory.decodeStream(new FileInputStream(new File(uri.getPath())), null, options);
+        } else {
             BitmapFactory.decodeStream(new FileInputStream(new File(uri.getPath())), null, options);
         }
 
@@ -59,19 +60,21 @@ public class BitmapUtil {
      * @param uri
      * @param bitmap
      * @return
-     * @throws IOException
+     * @throws java.io.IOException
      */
     private static Bitmap getRotatedBitmap(Context context, Uri uri, Bitmap bitmap) throws IOException {
         String realPath = null;
-        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
-        if (cursor == null) { // Source is Dropbox or other similar local file path
-            realPath = uri.getPath();
-        } else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            realPath = cursor.getString(idx);
-            cursor.close();
-        }
+//        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+//        if (cursor == null) { // Source is Dropbox or other similar local file path
+//            realPath = uri.getPath();
+//        } else {
+//            cursor.moveToFirst();
+//            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+//            realPath = cursor.getString(idx);
+//            cursor.close();
+//        }
+
+        realPath = uri.getPath();
 
         // 获取方向
         ExifInterface exif = new ExifInterface(realPath);
@@ -131,8 +134,39 @@ public class BitmapUtil {
 
     }
 
-    public static Bitmap zip(Resources res, int resId,
-                             int reqWidth, int reqHeight) {
+    public static Bitmap zip(Resources res, int resId, int size , int compressPercent, Bitmap.CompressFormat compressFormat) {
+        Bitmap resized = resize(res, resId, size, size);
+        return compress(resized, compressPercent, compressFormat);
+    }
+
+    public static Bitmap compress(Resources res, int resId, int compressPercent, Bitmap.CompressFormat compressFormat) {
+        Bitmap bitmap = BitmapFactory.decodeResource(res, resId);
+        return compress(bitmap, compressPercent, compressFormat);
+    }
+
+    public static Bitmap compress(Bitmap bitmap, int compressPercent, Bitmap.CompressFormat compressFormat) {
+        Bitmap compressed = null;
+        try {
+            ByteArrayOutputStream bucket = new ByteArrayOutputStream();
+            bitmap.compress(compressFormat, compressPercent, bucket);
+            ByteArrayInputStream bais = new ByteArrayInputStream(bucket.toByteArray());
+            compressed = BitmapFactory.decodeStream(bais);
+        } catch (Exception e) {
+            LogUtil.printStackTrace(e);
+        } catch (OutOfMemoryError e) {
+            LogUtil.printStackTrace(e);
+        }
+        try {
+            bitmap.recycle();
+            bitmap = null;
+        } catch (Exception e) {
+            LogUtil.printStackTrace(e);
+        }
+        return compressed;
+    }
+
+    public static Bitmap resize(Resources res, int resId,
+                                int reqWidth, int reqHeight) {
 
         // First decode with inJustDecodeBounds=true to check dimensions
         final BitmapFactory.Options options = new BitmapFactory.Options();
@@ -166,5 +200,57 @@ public class BitmapUtil {
         }
 
         return inSampleSize;
+    }
+
+
+    /**
+     * 将图片截取为圆角图片
+     *
+     * @param bitmap 原图片
+     * @param ratio  截取比例，如果是8，则圆角半径是宽高的1/8，如果是2，则是圆形图片
+     * @return 圆角矩形图片
+     */
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, float ratio) {
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawRoundRect(rectF, bitmap.getWidth() / ratio,
+                bitmap.getHeight() / ratio, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    /********
+     * 按短边截取正矩形
+     *
+     * @param bitmap
+     * @return
+     */
+    public static Bitmap getRectBySmall(Bitmap bitmap) {
+        int small = 0;
+        int x = 0;
+        int y = 0;
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            x = (bitmap.getWidth() - bitmap.getHeight()) / 2;
+            y = 0;
+            small = bitmap.getHeight();
+        } else {
+            x = 0;
+            y = (bitmap.getHeight() - bitmap.getWidth()) / 2;
+            small = bitmap.getWidth();
+        }
+
+
+        return Bitmap.createBitmap(bitmap, x, y, small, small);
     }
 }
